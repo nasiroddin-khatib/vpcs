4f81e45b9aa22fda66595df7585db8dcfa152019
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-09-05 12:31:22 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://Backend-LB-935014965.ap-south-1.elb.amazonaws.com",
+    origin: "http://13.200.251.35",
     methods: ["GET", "POST"],
     credentials: true
   })
